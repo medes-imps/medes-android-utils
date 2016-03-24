e1b3c6a525e620e1a436a13b9a0af4a028f82ecd
Julien Dupouy <julien.dupouy@medes.fr>
2016-03-24 19:15:11 +0100

    Add progress dialog fragment Use compatibility library for preferences

---
+++ src/main/java/fr/medes/android/app/ProgressDialogFragment.java
@@ -0,0 +1,78 @@
+package fr.medes.android.app;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+public class ProgressDialogFragment extends DialogFragment {
+
+	public static final String ARG_TITLE = "title";
+	public static final String ARG_MESSAGE = "message";
+	public static final String ARG_MAX = "max";
+	public static final String ARG_CANCELABLE = "cancelable";
+	public static final String ARG_STYLE = "style";
+
+	public static ProgressDialogFragment newInstance() {
+		return new ProgressDialogFragment();
+	}
+
+	@NonNull
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		Bundle args = getArguments();
+		boolean cancelable = args.getBoolean(ARG_CANCELABLE, false);
+		String title = args.getString(ARG_TITLE);
+		String message = args.getString(ARG_MESSAGE);
+		int style = args.getInt(ARG_STYLE, ProgressDialog.STYLE_HORIZONTAL);
+		int max = args.getInt(ARG_MAX);
+
+		setCancelable(cancelable);
+
+		ProgressDialog dialog = new ProgressDialog(getActivity());
+		dialog.setTitle(title);
+		dialog.setMessage(message);
+		dialog.setIndeterminate(false);
+		dialog.setProgressStyle(style);
+		dialog.setMax(max);
+
+		return dialog;
+	}
+
+	public void setProgress(int value) {
+		ProgressDialog dialog = (ProgressDialog) getDialog();
+		if (dialog != null) {
+			dialog.setProgress(value);
+		}
+	}
+
+	@Override
+	public void onCancel(DialogInterface dialog) {
+		super.onCancel(dialog);
+		Callback callback = getProgressDialogFragmentCallback();
+		if (callback != null) {
+			callback.onCancel();
+		}
+	}
+
+	private Callback getProgressDialogFragmentCallback() {
+		Activity activity = getActivity();
+		if (activity == null) {
+			return null;
+		}
+
+		if (activity instanceof Callback) {
+			return (Callback) activity;
+		}
+		return null;
+	}
+
+	public interface Callback {
+		void onCancel();
+	}
+
+}
--- src/main/java/fr/medes/android/preference/MyEditTextPreference.java
+++ src/main/java/fr/medes/android/preference/MyEditTextPreference.java
@@ -1,19 +1,23 @@
 package fr.medes.android.preference;
 
 import android.content.Context;
-import android.preference.EditTextPreference;
 import android.preference.Preference;
+import android.support.v7.preference.EditTextPreference;
 import android.util.AttributeSet;
 
 /**
  * A {@link Preference} that allows for string input.
- * <p>
+ * <p/>
  * The string input is displayed in the summary.
  */
 public class MyEditTextPreference extends EditTextPreference {
 
 	public MyEditTextPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
+	}
+
+	public void updateSummary() {
+		setSummary(getPersistedString(getContext().getString(android.R.string.unknownName)));
 	}
 
 	@Override
